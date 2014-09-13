d74721b817543b3fe7df246580d2a6922d0dae00
frenchjam <joe.mcintyre@parisdescartes.fr>
2014-09-13 13:32:16 +0200

    Reorganization of keyboard actions.
    <Shift> to rotate the head orientation.
    <Ctrl> to move the hand.
    <Alt><Ctrl> to rotate the hand.
    Mouse can be used to move up, down, left, right and to rotate around
    the roll axis.
    ‘L’ lowers the hand. ‘R’ raises the hand.

--- PsyPhy/MoveByKeysMono.cs
+++ PsyPhy/MoveByKeysMono.cs
@@ -21,6 +21,10 @@ public class MoveByKeysMono : MonoBehaviour
 	public KeyCode inward = KeyCode.Q;
 	public KeyCode outward = KeyCode.S;
 
+	public bool use_mouse = false;
+	public float previous_mouse_x;
+	public float previous_mouse_y;
+
 
 	// These probably exist as globals or elements of some class, but I don't know where.
 	// It was easier to define them here than to look any further.
@@ -33,56 +37,64 @@ public class MoveByKeysMono : MonoBehaviour
 
 	private bool stillDown = false;
 	
+	private float delta_x, delta_y;
+	
 	void Update ()
 	{ 
 
-				if (Shift && !(Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)))
-						return;
-				if (Control && !(Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)))
-						return;
+		if ((Shift && !(Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))) ||
+			(Control && !(Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)))) {
 
-				// Spacebar resets to starting view.
-				if (Input.GetKey (KeyCode.Space)) {
-						this.transform.rotation = startRotation;
-						this.transform.position = startPosition;
-				}
-				if (Input.GetKey (KeyCode.D)) {
-						if (mode == DriveMode.rotate && !stillDown) mode = DriveMode.translate;
-						else if ( mode == DriveMode.translate && !stillDown) mode = DriveMode.rotate;
-						stillDown = true;
-				} else stillDown = false;
+			// If the modifiers are not pressed as needed, just record the current mouse position for the next time through.
+			previous_mouse_x = Input.mousePosition.x;
+			previous_mouse_y = Input.mousePosition.y;
+			return;
 
-				if (mode == DriveMode.rotate || (mode == DriveMode.translate && (Input.GetKey (KeyCode.LeftAlt) || Input.GetKey (KeyCode.RightAlt)))) {
-								// Look around to the left, right, up and down directions.
-						if (Input.GetKey (left)) 
-								this.transform.Rotate (j_vector, - rotateSpeed * Time.deltaTime, Space.Self);
-						if (Input.GetKey (right))
-							this.transform.Rotate (j_vector, rotateSpeed * Time.deltaTime, Space.Self);
-						if (Input.GetKey (up))
-							this.transform.Rotate (i_vector, - rotateSpeed * Time.deltaTime, Space.Self);
-						if (Input.GetKey (down))
-							this.transform.Rotate (i_vector, rotateSpeed * Time.deltaTime, Space.Self);	
-						// Roll around the current heading.
-						if (Input.GetKey (inward))
-							this.transform.Rotate (k_vector, rotateSpeed * Time.deltaTime, Space.Self);
-						if (Input.GetKey (outward))
-							this.transform.Rotate (k_vector, - rotateSpeed * Time.deltaTime, Space.Self);
-						
-				} else {
-						if (Input.GetKey (left) )
-								this.transform.Translate (- translateSpeed * Time.deltaTime * i_vector, Space.Self);
-						if (Input.GetKey (right) )
-							this.transform.Translate (translateSpeed * Time.deltaTime * i_vector, Space.Self);
-						if (Input.GetKey (up)) 
-							this.transform.Translate (translateSpeed * Time.deltaTime * j_vector, Space.Self);
-						if (Input.GetKey (down) )
-							this.transform.Translate (- translateSpeed * Time.deltaTime * j_vector, Space.Self);
-						if (Input.GetKey (inward)) 
-							this.transform.Translate (- translateSpeed * Time.deltaTime * k_vector, Space.Self);
-						if (Input.GetKey (outward) )
-							this.transform.Translate (translateSpeed * Time.deltaTime * k_vector, Space.Self);
-						
-				}
+		}
+
+		// Spacebar resets to starting view.
+		if (Input.GetKey (KeyCode.Space)) {
+				this.transform.rotation = startRotation;
+				this.transform.position = startPosition;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+				if (mode == DriveMode.rotate && !stillDown) mode = DriveMode.translate;
+				else if ( mode == DriveMode.translate && !stillDown) mode = DriveMode.rotate;
+				stillDown = true;
+		} else stillDown = false;
+
+		// Update transform according to mode and which modifiers are pressed.
+		// Alt key allows you to override to the other mode.
+		if (mode == DriveMode.rotate || (mode == DriveMode.translate && (Input.GetKey (KeyCode.LeftAlt) || Input.GetKey (KeyCode.RightAlt)))) {
+			if (Input.GetKey (left))  this.transform.Rotate (j_vector, - rotateSpeed * Time.deltaTime, Space.Self);
+			if (Input.GetKey (right)) this.transform.Rotate (j_vector, rotateSpeed * Time.deltaTime, Space.Self);
+			if (Input.GetKey (up)) this.transform.Rotate (i_vector, - rotateSpeed * Time.deltaTime, Space.Self);
+			if (Input.GetKey (down)) this.transform.Rotate (i_vector, rotateSpeed * Time.deltaTime, Space.Self);	
+			if (Input.GetKey (inward)) this.transform.Rotate (k_vector, rotateSpeed * Time.deltaTime, Space.Self);
+			if (Input.GetKey (outward)) this.transform.Rotate (k_vector, - rotateSpeed * Time.deltaTime, Space.Self);
+			if ( use_mouse ) {	
+				delta_x = Input.mousePosition.x - previous_mouse_x;	
+				this.transform.Rotate ( k_vector, delta_x / 50.0f, Space.Self );	
+			}
+		} else {
+			if (Input.GetKey (left) ) this.transform.Translate (- translateSpeed * Time.deltaTime * i_vector, Space.Self);
+			if (Input.GetKey (right) ) this.transform.Translate (translateSpeed * Time.deltaTime * i_vector, Space.Self);
+			if (Input.GetKey (up)) this.transform.Translate (translateSpeed * Time.deltaTime * j_vector, Space.Self);
+			if (Input.GetKey (down) ) this.transform.Translate (- translateSpeed * Time.deltaTime * j_vector, Space.Self);
+			if (Input.GetKey (inward)) this.transform.Translate (- translateSpeed * Time.deltaTime * k_vector, Space.Self);
+			if (Input.GetKey (outward) ) this.transform.Translate (translateSpeed * Time.deltaTime * k_vector, Space.Self);
+			delta_x = Input.mousePosition.x - previous_mouse_x;	
+			delta_y = Input.mousePosition.y - previous_mouse_y;	
+			if ( use_mouse ) {	
+				this.transform.Translate ( delta_x / 100.0f * i_vector, Space.Self );	
+				this.transform.Translate ( delta_y / 100.0f * j_vector, Space.Self );
+			}
+		}
+		
+		// Save mouse position for next time.
+		previous_mouse_x = Input.mousePosition.x;
+		previous_mouse_y = Input.mousePosition.y;
+		
 	}
 
 	void Start()
@@ -90,5 +102,10 @@ public class MoveByKeysMono : MonoBehaviour
 		// Store the pose at onset, to be used to reset later.
 		startRotation = this.transform.rotation;
 		startPosition = this.transform.position;
+		
+		// Initialize the mouse position, so that we can calculate deltas later.
+		previous_mouse_x = Input.mousePosition.x;
+		previous_mouse_y = Input.mousePosition.y;
+		
 	}
 }
